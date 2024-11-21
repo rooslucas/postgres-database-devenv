daf295c0ccdb01521d60d6eb1a70fbe2324144fc
rooslucas <rooslucas@live.nl>
2024-11-21 12:16:11 +0100

    Complete functions

--- functions.py
+++ functions.py
@@ -16,19 +16,52 @@ def connect():
     
     return connection, cursor
 
-def create_list():
+def create_list(list_name: str):
     connection, cursor = connect()
+    try:
+        cursor.execute(f"CREATE TABLE {list_name} (ID INT PRIMARY KEY, ITEM TEXT)")
+    
+    except:
+        print("This list was already created")
+    
+    connection.commit()
+    connection.close()
+
     print("List Created")
 
-def add_item():
-    connection, cursor = connect()
+def add_item(db, id, item):
+    connection, cursor = connect()   
+
+    try:  
+        cursor.execute(f"INSERT INTO {db} (ID, ITEM) VALUES ({id}, {item})")
+    except:
+        print(f"This item was already added")
+    
+    connection.commit()
+    connection.close()
+
     print("Added")
 
-def remove_item():
+def remove_item(db, id):
     connection, cursor = connect()
+
+    try:  
+        cursor.execute(f"DELETE FROM {db} WHERE ID={id}")
+    except:
+        print(f"This item was already removed")
+
+    connection.commit()
+    connection.close()
+
     print("Removed")
 
-def view():
+def view(db):
     connection, cursor = connect()
+
+    cursor.execute(f"SELECT * FROM {db}")
+        
+    connection.commit()
+    connection.close()
+
     print("View")
 
